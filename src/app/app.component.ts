6c965a1502f8a281ee1fe3ecdaa25660d64b1fe0
Shalitha Vikum <hsvikum@gmail.com>
2021-02-08 12:44:53 +0530

    Inital Employee List and Detail Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-info';
+
+  emptyEmployee = {
+    "empNumber": "",
+    "employeeId": "",
+    "firstName": "",
+    "lastName": "",
+    "middleName": "",
+    "dateOfBirth": "",
+    "maritalStatus": false,
+    "gender": 'male'
+  };
+
+  currentEmployee = null;
+
+  employees = [
+    {
+      "empNumber": "54",
+      "employeeId": "EMP-54",
+      "firstName": "Rachel",
+      "lastName": "Alexander",
+      "middleName": "",
+      "dateOfBirth": "2012-04-19",
+      "maritalStatus": true,
+      "gender": 'male'
+    },
+    {
+      "empNumber": "66",
+      "employeeId": "EMP-66",
+      "firstName": "Abbie",
+      "lastName": "Campbell",
+      "middleName": "Snider",
+      "dateOfBirth": "1991-04-19",
+      "maritalStatus": true,
+      "gender": 'female'
+    },
+    {
+      "empNumber": "77",
+      "employeeId": "EMP-77",
+      "firstName": "Ricardo",
+      "lastName": "Bowman",
+      "middleName": "",
+      "dateOfBirth": "2001-04-19",
+      "maritalStatus": false,
+      "gender": 'male'
+    }
+  ];
+
+  selectEmployee(employee) {
+    this.currentEmployee = employee;
+  }
+
+  cancelEmployeeDetails() {
+    this.currentEmployee = null;
+  }
+
+  saveEmployeeDetails() {
+
+  }
+
+  resetEmployeeDetails() {
+    this.currentEmployee = this.emptyEmployee;
+  }
 }
